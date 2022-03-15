676990935c2251bb47471195293f183fe1efb878
Roan-Schellingerhout <roan.schellingerhout@student.uva.nl>
2022-03-15 13:03:29 +0100

    cleaning up repo, added TMDB data + embeddings

--- imdb/add_ENvsNonEN_feature.py
+++
@@ -1,9 +0,0 @@
-def add_english_title_or_not(df_):
-    '''Create a column indicating whether the title of the movie is english or not, simplifying the english column.'''
-    
-    assert 'title_language' in df_.columns, 'title of language has not yet been distilled'
-    
-    df_['isEN'] = False
-    df_.loc[df_['title_language'] == 'en', 'isEN'] = True
-    
-    return df_
--- imdb/add_remake_feature.py
+++
@@ -1,7 +0,0 @@
-def create_remake_column(df_):
-    ''''Create column that indicates whether the movie is a remake''' 
-    all_dups = df_.duplicated(subset='primaryTitle', keep=False)
-    df_['hasRemake'] = False
-    df_.loc[all_dups, 'hasRemake'] = True 
-    
-    return df_
