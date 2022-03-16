4e4af0aa6773cd3e4a96041c4f89cfd600ec2200
rvdb7345 <rvdb7345@gmail.com>
2022-03-16 12:57:15 +0000

    added the functions for the addition of the title language

--- imdb/py_files/add_langoriginaltitle_feature.py
+++ imdb/py_files/add_langoriginaltitle_feature.py
@@ -1,34 +1,35 @@
+import tqdm
 from langdetect import detect
 
 def add_language_of_original_title(df_):
     '''Create column indicating the language of the available title.''' 
     
     # format the primary title so that it is plain but usable for language detection
-    df_["primaryFormattedTitle"] = df_["primaryTitle"].str.lower()\
+    df_["temp_prim_title"] = df_["primaryTitle"].str.lower()\
                                        .str.normalize('NFKD')\
                                        .str.encode('ascii', errors='ignore')\
                                        .str.decode('utf-8')
     
     # make sure the original title is a string and not an object
-    df_['originalFormattedTitle'] = df_['originalTitle'].astype(str)
+    df_['temp_orig_title'] = df_['originalTitle'].astype(str)
     df_['title_language'] = 'unknown'
 
     # try to get the language for the original title for every row, otherwise get the language of the primary title
     for idx, row in tqdm.tqdm(df_.iterrows()):
-        if df_.loc[idx, 'originalFormattedTitle'] != 'nan':
+        if df_.loc[idx, 'temp_orig_title'] != 'nan':
             try:
-                df_.loc[idx, 'title_language'] = detect(df_.loc[idx]['originalFormattedTitle'])
+                df_.loc[idx, 'title_language'] = detect(df_.loc[idx]['temp_orig_title'])
             except:
-                print(f"Could not detect language of original title: {df_.loc[idx]['originalFormattedTitle']}")
+                print(f"Could not detect language of original title: {df_.loc[idx]['temp_orig_title']}")
                 df_.loc[idx, 'title_language'] = 'unknown'
         else:
             try:
-                df_.loc[idx, 'title_language'] = detect(df_.loc[idx]['primaryFormattedTitle'])
+                df_.loc[idx, 'title_language'] = detect(df_.loc[idx]['temp_prim_title'])
             except:
-                print(f"Could not detect language of primary title: {df_.loc[idx]['primaryFormattedTitle']}")
+                print(f"Could not detect language of primary title: {df_.loc[idx]['temp_prim_title']}")
                 df_.loc[idx, 'title_language'] = 'unknown'
                 
-    df_.drop(['primaryFormattedTitle', 'originalFormattedTitle'], axis=1, inplace=True)
+    df_.drop(['temp_prim_title', 'temp_orig_title'], axis=1, inplace=True)
     
     return df_
                 
--- imdb/py_files/add_merge_begin_end_year.py
+++ imdb/py_files/add_merge_begin_end_year.py
@@ -1,3 +1,3 @@
 def merge_start_end_year(df_):
-    df_['year'] = df_['startYear'].fillna(df_['endYear'])
+    df_['Year'] = df_['startYear'].fillna(df_['endYear'])
     return df_
